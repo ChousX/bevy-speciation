896a9e8527a7d1869bb005648f48705ff6885371
A. Garrett Gelwick III <gmanenergy@gmail.com>
2025-12-03 11:14:13 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "bevy-speaciation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    fenix,
+    systems,
+    ...
+  }: let
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            fenix.overlays.default
+          ];
+        };
+
+        rust-toolchain = pkgs.fenix.stable.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ];
+
+        bevy-runtime-deps = with pkgs; [
+          pkg-config
+          alsa-lib
+          vulkan-tools
+          vulkan-headers
+          vulkan-loader
+          vulkan-validation-layers
+          libudev-zero
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          xorg.libxcb
+          libxkbcommon
+          wayland
+        ];
+
+        bevy-build-deps = with pkgs; [
+          clang
+          lld
+        ];
+      in
+        f {
+          inherit pkgs;
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+
+          all-deps = bevy-build-deps ++ bevy-runtime-deps;
+        });
+  in {
+    formatter = forEachSystem ({pkgs, ...}: pkgs.alejandra);
+
+    devShells = forEachSystem ({
+      pkgs,
+      craneLib,
+      all-deps,
+      ...
+    }: {
+      default = craneLib.devShell {
+        packages = all-deps;
+        shellHook = ''
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath (with pkgs; [
+            libxkbcommon
+            vulkan-loader
+          ])}"
+        '';
+      };
+    });
+    packages = forEachSystem ({
+      craneLib,
+      all-deps,
+      ...
+    }: {
+      default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./.;
+        nativeBuildInputs = all-deps;
+      };
+    });
+  };
+}
